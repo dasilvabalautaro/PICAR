2b64ea157106ccd0be9338607d15577219ac2b2b
arturosilvabaldellon <david.arturo.silva@gmail.com>
2018-09-30 10:37:32 -0400

    Initial commit

---
+++ app/src/main/java/com/empoderar/picar/presentation/view/activities/TaskActivity.kt
@@ -0,0 +1,13 @@
+package com.empoderar.picar.presentation.view.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.empoderar.picar.R
+
+class TaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+    }
+}
