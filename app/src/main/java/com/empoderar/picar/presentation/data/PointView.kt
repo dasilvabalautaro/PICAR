eb63d737a2faa1d7fc6bbf300c60a12b9e27bd4c
arturosilvabaldellon <david.arturo.silva@gmail.com>
2018-10-30 12:10:49 -0400

    Point location

---
+++ app/src/main/java/com/empoderar/picar/presentation/data/PointView.kt
@@ -0,0 +1,25 @@
+package com.empoderar.picar.presentation.data
+
+import android.os.Parcel
+import com.empoderar.picar.presentation.plataform.*
+
+data class PointView(var latitude: Double,
+                     var longitude: Double,
+                     var title: String): KParcelable  {
+
+    companion object {
+        @JvmField val CREATOR = parcelableCreator(
+                ::PointView)
+    }
+
+    constructor(parcel: Parcel) : this(parcel.readDouble(), parcel.readDouble(),
+            parcel.readString())
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        with(dest) {
+            writeDouble(latitude)
+            writeDouble(longitude)
+            writeString(title)
+        }
+    }
+}
