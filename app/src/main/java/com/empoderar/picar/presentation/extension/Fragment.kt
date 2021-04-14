ae4b3b7a733710bc0a5a991d96d85d6731e13cc8
arturosilvabaldellon <david.arturo.silva@gmail.com>
2021-04-14 11:05:14 -0400

    Graphics library all

--- app/src/main/java/com/empoderar/picar/presentation/extension/Fragment.kt
+++ app/src/main/java/com/empoderar/picar/presentation/extension/Fragment.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.empoderar.picar.presentation.extension
 
 import androidx.lifecycle.ViewModel
@@ -12,6 +14,7 @@ import androidx.fragment.app.FragmentTransaction
 import com.empoderar.picar.presentation.plataform.BaseActivity
 import com.empoderar.picar.presentation.plataform.BaseFragment
 import kotlinx.android.synthetic.main.activity_task.*
+
 
 inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) =
         beginTransaction().func().commit()
